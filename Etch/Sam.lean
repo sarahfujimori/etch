52af21e19451032e4cbc8fdc5fb01d927c03d7a0
Sarah Fujimori <sfuji@stanford.edu>
2024-04-04 21:51:49 -0700

    rename main file
    - Add ofNat instance to make testing easier
    - Generalize Token add, mul and ord for other base types
    - Change context to store level data instead of containing functions to access data
    - Reorder some code
    - Fix repeater implementation for correctness (previous was wrong)
    - Move level scan code out of eval function
    - Remove unused intersect code

--- Etch/Sam.lean
+++ Etch/Sam.lean
@@ -1,5 +1,16 @@
 import Mathlib.Tactic
 
+/- todo
+- Multi intersect/union
+- Ref/Crd/Val distinction
+- think about changing expr data structure
+- fix level scanner
+- Fix types to be different for values
+- Generalize vector reducer (type and dimension)
+- remove partial def on extendlist method
+- get rid of level number in level constructor
+-/
+
 namespace SAM
 
 inductive Token (ι : Type)
@@ -9,7 +20,10 @@ inductive Token (ι : Type)
 | empty
 deriving Repr
 
-def Token.comp : Token ℕ → Token ℕ → Ordering
+instance : OfNat (Token Nat) n where
+  ofNat := Token.val n
+
+def Token.comp [Ord ι]: Token ι → Token ι → Ordering
   | Token.empty, _ => Ordering.eq
   | _, Token.empty => Ordering.eq
   | Token.done, Token.done => Ordering.eq
@@ -20,11 +34,11 @@ def Token.comp : Token ℕ → Token ℕ → Ordering
   | Token.stop _, _ => Ordering.gt
   | Token.val v1, Token.val v2 => Ord.compare v1 v2
 
-instance : Ord (Token ℕ) where
+instance [Ord ι]: Ord (Token ι) where
   compare := Token.comp
 
 -- Treat empty tokens as zeros
-def Token.add: Token ℕ → Token ℕ → Token ℕ
+def Token.add [Add α]: Token α → Token α → Token α
   | Token.val v1, Token.val v2 => Token.val (v1 + v2)
   | Token.empty, Token.val v => Token.val v
   | Token.val v, Token.empty => Token.val v
@@ -32,7 +46,7 @@ def Token.add: Token ℕ → Token ℕ → Token ℕ
   | Token.done, Token.done => Token.done
   | _, _ => Token.empty --invalid
 
-def Token.mul: Token ℕ → Token ℕ → Token ℕ
+def Token.mul [OfNat α 0] [Mul α]: Token α → Token α → Token α
   | Token.val v1, Token.val v2 => Token.val (v1 * v2)
   | Token.empty, Token.val _ => Token.val 0
   | Token.val _, Token.empty => Token.val 0
@@ -40,19 +54,61 @@ def Token.mul: Token ℕ → Token ℕ → Token ℕ
   | Token.done, Token.done => Token.done
   | _, _ => Token.empty --invalid
 
-instance: Add (Token ℕ) where
+instance [Add α]: Add (Token α) where
   add := Token.add
 
-instance: Mul (Token ℕ) where
+instance [OfNat α 0] [Mul α]: Mul (Token α) where
   mul := Token.mul
+
+instance [OfNat α 0]: OfNat (Token α) 0 where
+  ofNat := Token.val 0
 
 variable {ι : Type} [Zero ι]
 
 abbrev Stream (ι : Type) := List (Token ι)
 
+
+inductive SAMStream (ι: Type) (α: Type)
+| ref (r: Stream ι)
+| crd (c: Stream ι)
+| val (v: Stream α)
+deriving Repr
+
 abbrev Ident := String
 
--- todo: `tree-like' structure
+inductive Level
+| null
+| dense (n: Nat) (size: Nat)
+| compressed (n: Nat) (v1: List Nat) (v2: List Nat)
+deriving Repr
+
+structure Ctxt (α: Type) (ι : Type) where
+levels: Ident → (List Level)
+val: Ident → (List α)
+
+#check Level.dense 0 3
+#check Level.compressed 1 [0, 3] [0, 3, 5]
+
+def emptyContext : Ctxt α ι where
+levels := fun _ => []
+val := fun _ => []
+
+def range (a b: ℕ) := List.range (b-a) |>.map (.+a)
+
+def getLevel (n: ℕ) (ident: Ident) (c: Ctxt α ι) : Level :=
+  if h: n < (c.levels ident).length then (c.levels ident)[n]'h else Level.null
+
+def getChildren (level: Level) (i: Nat): List ℕ × List ℕ :=
+match level with
+| Level.dense _ size => (range (size*i) (size*i+size), range 0 size)
+| Level.compressed _ v1 v2 => if h: (i+1) < v1.length
+                then have h': i < v1.length := by { exact Nat.lt_of_succ_lt h }
+                  if h2: v1[i+1] <= v2.length ∧ v1[i]'h' < v2.length
+                  then (range v1[i] v1[i+1], v2.toArray[v1[i]:v1[i+1]].toArray.toList)
+                  else ([], [])
+                else ([], [])
+| Level.null => ([], [])
+
 inductive Format
 inductive Expr
 | root
@@ -64,14 +120,9 @@ inductive Expr
 | intersect (input1: Expr) (input2: Expr) -- input1 and input2 are both (ref, crd) pairs; currently only binary intersect
 | union (input1: Expr) (input2: Expr) -- input1 and input2 are both (ref, crd) pairs; currently only binary union
 | repeater (inputc: Expr) (inputr: Expr) -- inputc is one crd stream, inputr is one ref stream
+--| vecreducer (input_c: Expr) (input_v: Expr) -- input/output 1 coordinate streams and one value stream
 
-structure Ctxt (ι : Type) where
- -- name -> level -> index -> (ref, crd) for list of children
-  level_data : Ident → Nat → ι → Prod (List ι) (List ι)
- -- name -> index -> val
-  val_data : Ident → ι → ι
-
-partial def binaryIntersectHelper [Ord (Token ι)] (xr: Stream ι) (xc: Stream ι) (yr: Stream ι)(yc: Stream ι) (res_c: Stream ι) (res_rx: Stream ι) (res_ry: Stream ι): Stream ι × Stream ι × Stream ι :=
+def binaryIntersectHelper [Ord (Token ι)] (xr: Stream ι) (xc: Stream ι) (yr: Stream ι)(yc: Stream ι) (res_c: Stream ι) (res_rx: Stream ι) (res_ry: Stream ι): Stream ι × Stream ι × Stream ι :=
 match xr, xc, yr, yc with
 | r_x::xs_r, c_x::xs_c, r_y::ys_r, c_y::ys_c =>
   match Ord.compare c_x c_y with
@@ -92,7 +143,6 @@ def BinaryIntersect [Ord (Token ι)] (r1: Stream ι) (c1: Stream ι) (r2: Stream
 -- todo
 def MultiIntersect (l: List (Stream ι × Stream ι)): Stream ι × (List (Stream ι)) := sorry
 
---todo: remove this partial def
 def binaryUnionHelper [Ord (Token ι)] (xr: Stream ι) (xc: Stream ι) (yr: Stream ι)(yc: Stream ι) (res_c: Stream ι) (res_rx: Stream ι) (res_ry: Stream ι): Stream ι × Stream ι × Stream ι :=
 match xr, xc, yr, yc with
 | r_x::xs_r, c_x::xs_c, r_y::ys_r, c_y::ys_c =>
@@ -121,53 +171,67 @@ def yc : Stream ℕ := [Token.val 4, Token.val 5, Token.stop 0, Token.done]
 #eval BinaryIntersect xr xc yr yc
 #eval BinaryUnion xr xc yr yc
 
-def repeaterHelper (crd: Stream ι) (ref: Stream ι) (m: ℕ): Stream ι :=
+def nonControlTokensHelper (s: Stream ι) (res: List ι) : List ι :=
+  match s with
+  | [] => res
+  | Token.val v::rest => nonControlTokensHelper rest (res ++ [v])
+  | _::rest => nonControlTokensHelper rest res
+
+def nonControlTokens (s: Stream ι) : List ι := nonControlTokensHelper s []
+
+def repeaterHelper (crd: Stream ι) (ref: List ι) (res: Stream ι) (i: ℕ): Stream ι :=
 match crd with
-| [] => [] -- This case should never happen since every stream has a done token
-| Token.val _::crds => repeaterHelper crds ref (m+1)
-| Token.empty::crds => repeaterHelper crds ref m     -- Ignore empty tokens
-| _::_ => let val : List (Stream ι) := ref.map fun token => match token with
-        | .val i => List.range m |>.map (fun _ => .val i)
-        | c => [c]
-      val.join
+| [] => [] -- Input error, no done token
+| Token.val _::rest => if h:i < ref.length then
+  repeaterHelper rest ref (res ++ [Token.val ref[i]]) i
+  else [] -- Input error
+| Token.empty::rest => repeaterHelper rest ref res i
+| Token.stop n::rest => match rest with
+   | Token.val _::_ => repeaterHelper rest ref (res ++ [Token.stop n]) (i+1)
+   | _ => repeaterHelper rest ref (res ++ [Token.stop n]) i
+| Token.done::_ => (res ++ [Token.done])
 
 -- Repeat each non-control token in ref m times, where m is number of non-control tokens in crd seen before a stop token is seen
+-- number of non-control tokens in ref stream is equal to number of control tokens in crd stream
+-- Output ref stream has same shape as input crd stream
 def Repeat (crd: Stream ι) (ref: Stream ι): Stream ι :=
-  repeaterHelper crd ref 0
+  repeaterHelper crd (nonControlTokens ref) [] 0
 
-#eval Repeat xc xr -- should be [0, 0, 1, 1, s0, done]
+#eval Repeat ([3, Token.stop 0, 4, 5, Token.stop 1, Token.done] : Stream Nat) ([0, 1, Token.stop 0, Token.done] : Stream Nat)
 
-def Expr.eval [Ord (Token ι)] [Mul (Token ι)] [Add (Token ι)] (ctxt : Ctxt ι) (e : Expr) : List (Stream ι) :=
+--#eval vecReduceHelper [.val 1, .val 2, .stop 0, .val 2, .done] [.val 3, .val 4, .stop 0, .val 3, .done] []
+
+def VecReduce [OfNat α 0] [Add α] (crds: Stream ℕ) (vals: Stream α) : Stream ℕ × Stream α := sorry
+
+--#eval VecReduce [.val 1, .val 2, .stop 0, .val 2, .done] [.val 3, .val 4, .stop 0, .val 3, .done]
+--#eval VecReduce ([3, 4, .stop 0, 4, .stop 1, 3, 4, .stop 1, 0, 5, .stop 2, .done]: Stream Nat) ([1, 2, .stop 0, 25, .stop 1, 3, 6, .stop 1, 54, 63, .stop 2, .done]: Stream Nat)
+
+def levelScanHelper (level: Level) (inputref: Stream ℕ) (res_r: Stream ℕ) (res_c: Stream ℕ): List (Stream ℕ) :=
+  match inputref with
+  | Token.done::_ => [res_r ++ [Token.done], res_c ++ [Token.done]]
+  | Token.val i::rest => match rest with -- If next token is a stop token, don't emit a stop 0
+      | Token.stop _::_ => levelScanHelper level rest
+            (res_r ++ ((getChildren level i).fst |>.map .val))
+            (res_c ++ ((getChildren level i).snd |>.map .val))
+      | _ => levelScanHelper level rest
+            (res_r ++ ((getChildren level i).fst |>.map .val) ++ [Token.stop 0])
+            (res_c ++ ((getChildren level i).snd |>.map .val) ++ [Token.stop 0])
+  | Token.stop n::rest => levelScanHelper level rest (res_r ++ [Token.stop (n+1)]) (res_c ++ [Token.stop (n+1)])
+  | Token.empty::rest => levelScanHelper level rest (res_r ++ [Token.stop 0]) (res_c ++ [Token.stop 0])
+  | [] => [] -- Invalid input
+
+def LevelScan (ctxt: Ctxt α ℕ) (name: Ident) (l: ℕ) (inputref: Stream ℕ) : List (Stream ℕ) :=
+  let level := getLevel l name ctxt
+  levelScanHelper level inputref [] []
+
+--def Expr.eval [OfNat ι 0] [Ord ι] [Mul ι] [Add ι] (ctxt : Ctxt α ι) (e : Expr) : List (Stream ι) :=
+def Expr.eval (ctxt: Ctxt ℕ ℕ) (e: Expr) : List (Stream ℕ) :=
 match e with
 | root => [[.val 0, .done]]
 | .scan name level input =>
   if h:(input.eval ctxt).length > 0
     then let is := (input.eval ctxt)[0]'h
-    -- Examples (Compressed m1):
-    -- is  = [0, done]
-    -- out = [0, 1, 2, s0, done] & [0, 3, 5, s0, done]
-
-    -- is = [0, 2, s0, done]
-    -- out = [0, 1, s0, 4, s1, done] & [1, 4, s0, 5, s1, done]
-
-    -- Examples (dense 3x4):
-    -- is = [0, 2, s0, done]
-    -- out = [0, 1, 2, 3, s0, 8, 9, 10, 11, s1, done] & [0, 1, 2, 3, 4, s0, 0, 1, 2, 3, 4, s1, done]
-
-    -- store previous token
-    let ref : List (Stream ι) := is.map fun token =>
-      match token with
-      | .val i => ((ctxt.level_data name level i).fst |>.map .val) ++ [Token.stop 0] -- ?: Don't add this to the last one?
-      | .stop n => [.stop (n+1)]
-      | .done => [.done]
-      | .empty => [.stop 0] -- ?
-    let crd : List (Stream ι) := is.map fun token =>
-      match token with
-      | .val i => ((ctxt.level_data name level i).snd |>.map .val) ++ [Token.stop 0] -- ?: Don't add this to the last one?
-      | .stop n => [.stop (n+1)]
-      | .done => [.done]
-      | .empty => [.stop 0] -- ?
-    [ref.join, crd.join]
+    LevelScan ctxt name level is
   else []
 | .add input1 input2 =>
   if h:(input1.eval ctxt).length > 0 ∧ (input2.eval ctxt).length > 0
@@ -192,10 +256,11 @@ match e with
 | .array_load name input =>
   if h:(input.eval ctxt).length > 0
     then let is := (input.eval ctxt)[0]'h
-      let val : List (Stream ι) := is.map fun token =>
+      let val : List (Stream ℕ) := is.map fun token =>
         match token with
-        | .val i => [Token.val (ctxt.val_data name i)] -- ?: Don't add this to the last one?
-        | c => [c]
+        | .val i => if h: i < (ctxt.val name).length then [Token.val (ctxt.val name)[i] ]
+          else []
+        | ctrl => [ctrl]
       [val.join]
     else []
 | select i input =>
@@ -230,87 +295,28 @@ match e with
        let ref := (inputr.eval ctxt)[0]'h.right
     [Repeat crd ref]
   else []
+/-|.vecreducer inputc inputv =>
+  if h:(inputc.eval ctxt).length > 0 ∧ (inputv.eval ctxt).length > 0
+  then let crd := (inputc.eval ctxt)[0]'h.left
+       let val := (inputv.eval ctxt)[0]'h.right
+       let result := VecReduce crd val
+      [result.fst, result.snd]
+  else []-/
 
-inductive Level
-| dense (n: Nat) (size: Nat)
-| compressed (n: Nat) (v1: List Nat) (v2: List Nat)
-deriving Repr
+def contextFromData (ident: Ident) (inputlevels: List Level) (inputvals: List Nat): Ctxt ℕ ℕ where
+  levels := fun name => if name = ident then inputlevels
+      else []
+  val := fun name  =>
+    if name = ident then inputvals
+    else []
 
-#check Level.dense 0 3
-#check Level.compressed 1 [0, 3] [0, 3, 5]
-
-def emptyContext : Ctxt ι where
-  level_data := fun _ _ _  => ([], [])
-  val_data := fun _ _ => 0
-
-def range (a b: ℕ) := List.range (b-a) |>.map (.+a)
-
-def contextFromData (ident: Ident) (levels: List Level) (vals: List Nat): Ctxt (ι := Nat) where
-  -- level_data : Ident → ι → List ι
-  -- ?: Write this function in a better way
-  level_data := fun name =>
-    if name = ident -- Check if name is valid
-      then (fun n =>
-        if h : n < levels.length -- Check if level number is valid
-          then fun i =>
-            match levels[n]'h with
-            | Level.dense _ size => (range (size*i) (size*i+size), range 0 size)
-            | Level.compressed _ v1 v2 =>
-              if h: (i+1) < v1.length
-                then have h': i < v1.length := by { exact Nat.lt_of_succ_lt h }
-                  if h2: v1[i+1] <= v2.length ∧ v1[i]'h' < v2.length
-                  then (range v1[i] v1[i+1], v2.toArray[v1[i]:v1[i+1]].toArray.toList)
-                  else ([], [])
-                else ([], [])
-          else fun _ => ([], []))
-      else fun _ _ => ([], [])
-  val_data := fun name  =>
-    if name = ident
-      then (fun i =>
-        if h: i < vals.length
-          then vals[i]
-          else 0
-      )
-    else 0
-
-def joinContexts (ident1: Ident) (c1: Ctxt (ι := Nat)) (ident2: Ident) (c2: Ctxt (ι := Nat)): Ctxt (ι:=Nat) where
-  level_data := fun name =>
-    if name = ident1 then c1.level_data name
-    else if name = ident2 then c2.level_data name
-    else fun _ _ => ([], [])
-  val_data := fun name =>
-    if name = ident1 then c1.val_data name
-    else if name = ident2 then c2.val_data name
-    else 0
-
+def joinContexts (ident1: Ident) (c1: Ctxt α ι) (ident2: Ident) (c2: Ctxt α ι): Ctxt α ι where
+levels := fun name =>
+    if name = ident1 then c1.levels ident1
+    else if name = ident2 then c2.levels ident2
+    else []
+val := fun name =>
+    if name = ident1 then c1.val ident1
+    else if name = ident2 then c2.val ident2
+    else []
 end SAM
-
-/-
-inductive StopCont
-| stop
-| cont
-deriving Repr
-
-partial def binaryIntersectHelper (xr: Stream ℕ) (xc: Stream ℕ) (a1: StopCont) (yr: Stream ℕ)(yc: Stream ℕ) (a2: StopCont) (res_c: Stream ℕ) (res_rx: Stream ℕ) (res_ry: Stream ℕ): Stream ℕ × Stream ℕ × Stream ℕ  :=
-match xr, xc, a1, yr, yc, a2 with
-| [], _, _, _, _, _
-| _, [], _, _, _, _
-| _, _, _, [], _, _
-| _, _, _, _, [], _
-| Token.done::_, _, _, _, _, _ => (res_c, res_rx, res_ry)
-| _, Token.done::_, _, _, _, _ => (res_c, res_rx, res_ry)
-| _, _, _, Token.done::_, _, _ => (res_c, res_rx, res_ry)
-| _, _, _, _, Token.done::_, _ => (res_c, res_rx, res_ry)
-| xs_r, xs_c, .stop, y::ys_r, Token.val v ::ys_c, .cont
-    => binaryIntersectHelper xs_r xs_c .stop ys_r ys_c .cont res_c res_rx res_ry
-| x::xs_r, Token.val v ::xs_c, .cont, ys_r, ys_c, .stop
-    => binaryIntersectHelper xs_r xs_c .cont ys_r ys_c .stop res_c res_rx res_ry
-| r_x::xs_r, Token.val v_x::xs_c, .cont, r_y::ys_r, Token.val v_y::ys_c, .cont
-    => if v_x < v_y
-      then binaryIntersectHelper xs_r xs_c .cont yr yc .cont res_c res_rx res_ry
-      else if v_x > v_y
-        then binaryIntersectHelper xr xc .cont ys_r ys_c .cont res_c res_rx res_ry
-        else binaryIntersectHelper xs_r xs_c .cont ys_r ys_c .cont (Token.val v_x::res_c) (r_x::res_rx) (r_y::res_ry)
---| xs, Token.stop n_x::xs_c, .cont, ys,
-| _, _, _, _, _, _ => ([], [], [])
--/
